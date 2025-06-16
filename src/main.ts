f472ae565eee24154d6a167d05174330ccd8c209
Omar Pintado <omarpintado98@gmail.com>
2025-06-15 22:15:58 -0500

    refactor: convert app to TCP microservice

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/env';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger: Logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
 
-  await app.listen(envs.port);
+  await app.listen();
 
-  logger.log(`App running on port: ${envs.port}`);
+  logger.log(`Products microservice running on port: ${envs.port}`);
 }
+
 bootstrap();
